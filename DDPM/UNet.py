5609654ac6cfb42f4b272bdbff2dea7941caa3ae
junyeong-nero <magicwho@unist.ac.kr>
2024-07-01 14:42:29 +0900

    [add] UNet.py

---
+++ DDPM/UNet.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        # Contracting path (Downsampling)
+        self.enc1 = self.double_conv(in_channels, 64)
+        self.enc2 = self.double_conv(64, 128)
+        self.enc3 = self.double_conv(128, 256)
+        self.enc4 = self.double_conv(256, 512)
+
+        # Bottleneck
+        self.bottleneck = self.double_conv(512, 1024)
+
+        # Expanding path (Upsampling)
+        self.upconv4 = self.up_conv(1024, 512)
+        self.dec4 = self.double_conv(1024, 512)
+        self.upconv3 = self.up_conv(512, 256)
+        self.dec3 = self.double_conv(512, 256)
+        self.upconv2 = self.up_conv(256, 128)
+        self.dec2 = self.double_conv(256, 128)
+        self.upconv1 = self.up_conv(128, 64)
+        self.dec1 = self.double_conv(128, 64)
+
+        # Output layer
+        self.out_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def double_conv(self, in_channels, out_channels):
+        """Two consecutive convolutional layers followed by ReLU"""
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def up_conv(self, in_channels, out_channels):
+        """Upsampling followed by convolution"""
+        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+    def forward(self, x):
+        # Contracting path
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(F.max_pool2d(enc1, kernel_size=2))
+        enc3 = self.enc3(F.max_pool2d(enc2, kernel_size=2))
+        enc4 = self.enc4(F.max_pool2d(enc3, kernel_size=2))
+
+        # Bottleneck
+        bottleneck = self.bottleneck(F.max_pool2d(enc4, kernel_size=2))
+
+        # Expanding path
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, self.crop_tensor(enc4, dec4)), dim=1)
+        dec4 = self.dec4(dec4)
+
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, self.crop_tensor(enc3, dec3)), dim=1)
+        dec3 = self.dec3(dec3)
+
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, self.crop_tensor(enc2, dec2)), dim=1)
+        dec2 = self.dec2(dec2)
+
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, self.crop_tensor(enc1, dec1)), dim=1)
+        dec1 = self.dec1(dec1)
+
+        out = self.out_conv(dec1)
+        return out
+
+    def crop_tensor(self, encoder_tensor, decoder_tensor):
+        """Crop the encoder tensor to match the shape of the decoder tensor"""
+        _, _, H, W = decoder_tensor.size()
+        enc_H, enc_W = encoder_tensor.size()[2], encoder_tensor.size()[3]
+        crop_H, crop_W = (enc_H - H) // 2, (enc_W - W) // 2
+        return encoder_tensor[:, :, crop_H:crop_H + H, crop_W:crop_W + W]
+
+
+if __name__ == '__main__':
+    # 모델 초기화 예시
+    unet = UNet(in_channels=1, out_channels=1)
+
+    # 임의의 입력 예시
+    x = torch.randn(1, 1, 28, 28)  # Batch size = 1, 채널 수 = 1, 이미지 크기 = 28x28
+    output = unet(x)
+
+    print(output.shape)  # torch.Size([1, 1, 28, 28])
