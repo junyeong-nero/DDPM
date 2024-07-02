27012990b9577f879de5ab23477f2618d34e11e7
junyeong-nero <magicwho@unist.ac.kr>
2024-07-02 14:34:38 +0900

    [update] Unet을 32 by 32 사이즈에 맞추어 업데이트

--- myDDPM/UNet.py
+++ myDDPM/UNet.py
@@ -22,33 +22,37 @@ class UNet(nn.Module):
         self.time_embed.requires_grad_(False)
 
         # Contracting path (Downsampling)
-        self.enc1 = self.double_conv(in_channels, 64)
         self.te_enc1 = self._make_time_embedding(time_emb_dim, 1)
+        self.enc1 = self.double_conv(in_channels, 64)
         
-        self.enc2 = self.double_conv(64, 128)
         self.te_enc2 = self._make_time_embedding(time_emb_dim, 64)
+        self.enc2 = self.double_conv(64, 128)
         
-        self.enc3 = self.double_conv(128, 256)
         self.te_enc3 = self._make_time_embedding(time_emb_dim, 128)
+        self.enc3 = self.double_conv(128, 256)
         
-        self.enc4 = self.double_conv(256, 512)
         self.te_enc4 = self._make_time_embedding(time_emb_dim, 256)
+        self.enc4 = self.double_conv(256, 512)
         
-        self.enc5 = self.double_conv(512, 1024)
-        self.te_enc5 = self._make_time_embedding(time_emb_dim, 512)
-
         # Bottleneck
-        self.bottleneck = self.double_conv(1024, 2048)
-        self.te_bottleneck = self._make_time_embedding(time_emb_dim, 1)
+        
+        self.te_bottleneck = self._make_time_embedding(time_emb_dim, 512)
+        self.bottleneck = self.double_conv(512, 1024)
 
         # Expanding path (Upsampling)
+        self.te_dec4 = self._make_time_embedding(time_emb_dim, 1024)
         self.upconv4 = self.up_conv(1024, 512)
         self.dec4 = self.double_conv(1024, 512)
         
+        self.te_dec3 = self._make_time_embedding(time_emb_dim, 512)
         self.upconv3 = self.up_conv(512, 256)
         self.dec3 = self.double_conv(512, 256)
+        
+        self.te_dec2 = self._make_time_embedding(time_emb_dim, 256)
         self.upconv2 = self.up_conv(256, 128)
         self.dec2 = self.double_conv(256, 128)
+        
+        self.te_dec1 = self._make_time_embedding(time_emb_dim, 128)
         self.upconv1 = self.up_conv(128, 64)
         self.dec1 = self.double_conv(128, 64)
 
@@ -70,39 +74,37 @@ class UNet(nn.Module):
 
     def forward(self, x, t=0):
         
-        # TODO : add timestep t into UNet.
+        # batch size
         t = self.time_embed(t)
         n = len(x)
         
         # Contracting path
-        enc1 = self.enc1(x)  # (B, 64, 32, 32)
-        enc2 = self.enc2(F.max_pool2d(enc1, kernel_size=2))  # (B, 128, 16, 16)
-        enc3 = self.enc3(F.max_pool2d(enc2, kernel_size=2))  # (B, 256, 8, 8)
-        enc4 = self.enc4(F.max_pool2d(enc3, kernel_size=2))  # (B, 512, 4, 4)
-        enc5 = self.enc5(F.max_pool2d(enc4, kernel_size=2))  # (B, 1024, 2, 2)
-
+        enc1 = self.enc1(x + self.te_enc1(t).reshape(n, -1, 1, 1))  # (B, 64, 32, 32)
+        enc2 = self.enc2(F.max_pool2d(enc1, kernel_size=2) + self.te_enc2(t).reshape(n, -1, 1, 1))  # (B, 128, 16, 16)
+        enc3 = self.enc3(F.max_pool2d(enc2, kernel_size=2) + self.te_enc3(t).reshape(n, -1, 1, 1))  # (B, 256, 8, 8)
+        enc4 = self.enc4(F.max_pool2d(enc3, kernel_size=2) + self.te_enc4(t).reshape(n, -1, 1, 1))  # (B, 512, 4, 4)
+        
         # Bottleneck
-        bottleneck = self.bottleneck(F.max_pool2d(enc5, kernel_size=2))  # (B, 2048, 1, 1)
+        bottleneck = self.bottleneck(F.max_pool2d(enc4, kernel_size=2) + self.te_bottleneck(t).reshape(n, -1, 1, 1))  # (B, 1024, 2, 2)
 
         # Expanding path
         dec4 = self.upconv4(bottleneck)  # (B, 512, 2, 2)
-        dec4 = torch.cat((dec4, self.center_crop(enc4, dec4.size(2), dec4.size(3))), dim=1)
+        dec4 = torch.cat((dec4, enc4), dim=1)
         dec4 = self.dec4(dec4)  # (B, 512, 2, 2)
+        
+        dec3 = self.upconv3(dec4)  # (B, 512, 2, 2)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.dec3(dec3)  # (B, 512, 2, 2)
+        
+        dec2 = self.upconv2(enc3)  # (B, 512, 2, 2)
+        dec2= torch.cat((dec2, enc2), dim=1)
+        dec2 = self.dec2(dec2)  # (B, 512, 2, 2)
+        
+        dec1 = self.upconv1(enc2)  # (B, 512, 2, 2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.dec1(dec1)  # (B, 512, 2, 2)
 
-        dec3 = self.upconv3(dec4)  # (B, 256, 4, 4)
-        dec3 = torch.cat((dec3, self.center_crop(enc3, dec3.size(2), dec3.size(3))), dim=1)
-        dec3 = self.dec3(dec3)  # (B, 256, 4, 4)
-
-        dec2 = self.upconv2(dec3)  # (B, 128, 8, 8)
-        dec2 = torch.cat((dec2, self.center_crop(enc2, dec2.size(2), dec2.size(3))), dim=1)
-        dec2 = self.dec2(dec2)  # (B, 128, 8, 8)
-
-        dec1 = self.upconv1(dec2)  # (B, 64, 16, 16)
-        dec1 = torch.cat((dec1, self.center_crop(enc1, dec1.size(2), dec1.size(3))), dim=1)
-        dec1 = self.dec1(dec1)  # (B, 64, 16, 16)
-
-        # Final upsampling to get 28x28 output
-        output = F.interpolate(dec1, size=(28, 28), mode='bilinear', align_corners=False)
+        output = F.interpolate(dec1, size=(32, 32), mode='bilinear', align_corners=False)
         output = self.out_conv(output)
         return output
 
@@ -119,10 +121,10 @@ class UNet(nn.Module):
 
 if __name__ == '__main__':
     # 모델 초기화 예시
-    unet = UNet(in_channels=1, out_channels=1)
+    unet = UNet(in_channels=1, out_channels=1, n_steps=1000)
 
     # 임의의 입력 예시
-    x = torch.randn(1, 1, 28, 28)  # Batch size = 1, 채널 수 = 1, 이미지 크기 = 28x28
-    output = unet(x)
+    x = torch.randn(1, 1, 32, 32)  # Batch size = 64, 채널 수 = 1, 이미지 크기 = 28x28
+    output = unet(x, torch.tensor(2))
 
     print(output.shape)  # torch.Size([1, 1, 28, 28])
