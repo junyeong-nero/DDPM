19dd460cfe36b636f0f2f961764cb65203530f77
junyeong-nero <magicwho@unist.ac.kr>
2024-07-17 12:53:55 +0900

    [ReverseDecoder] DDIM 로직 수정

--- myDDPM/ReverseDecoder.py
+++ myDDPM/ReverseDecoder.py
@@ -1,5 +1,7 @@
 import torch
 
+import torch
+
 class ReverseDecoder:
 
     def __init__(self, noise_schedule, g) -> None:
@@ -37,32 +39,35 @@ class ReverseDecoder:
 
         return noise_data
 
-    def implicit_denoise(self, noise_data, time_step, n_jumps=10):
+    def implicit_denoise(self, noise_data, time_step, n_jumps=10, sigma=0):
         # noise_data : [B, 1, 32, 32]
         # time_step : INT
 
         batch_size = noise_data.shape[0]
         gap = (time_step - 1) // n_jumps
         tau = [time_step - 1 - t * gap for t in range(n_jumps)]
-        print(tau)
+        S = len(tau)
+        # print(tau)
 
         # batch_size : B
         with torch.no_grad():
 
             # step : [T - 1, T - 2, .. 2, 1, 0]
-            for i in range(len(tau)):
+            for i in range(S):
 
                 t = torch.full((batch_size, ), tau[i])
                 t = t.reshape(-1, 1, 1, 1)
-
-                t_1 = torch.full((batch_size, ), tau[i - 1])
-                t_1 = t_1.reshape(-1, 1, 1, 1)
-                
                 alpha_t = self.noise_schedule._alphas[t]
-                alpha_t_1 = self.noise_schedule._alphas[t_1] if i > 0 else torch.full((batch_size, ), 1)
 
-                first = torch.sqrt(alpha_t / alpha_t_1) * (noise_data - torch.sqrt(1 - alpha_t) * self.g(noise_data, t))
-                second = noise_data - torch.sqrt(1 - alpha_t_1) * self.g(noise_data, t)
+                alpha_t_1 = torch.full((batch_size, 1, 1, 1,), 1)
+                if i < S - 1:
+                    t_1 = torch.full((batch_size, ), tau[i + 1])
+                    t_1 = t_1.reshape(-1, 1, 1, 1)
+                    alpha_t_1 = self.noise_schedule._alphas[t_1]
+
+                predict_noise = self.g(noise_data, t)
+                first = torch.sqrt(alpha_t / alpha_t_1) * (noise_data - torch.sqrt(1 - alpha_t) * predict_noise)
+                second = torch.sqrt(1 - alpha_t_1) * predict_noise
 
                 noise_data = first + second
 
