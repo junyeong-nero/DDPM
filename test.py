d31dc24ec9b65fa7d374843f3ec9b41b206cbe86
junyeong-nero <magicwho@unist.ac.kr>
2024-07-28 21:38:04 +0900

    colab 파일 추가, 불필요한 파일들 제거

--- test.py
+++
@@ -1,9 +0,0 @@
-import torch
-from myDiffusion.modules.UNet import SelfAttentionBlock
-
-block = SelfAttentionBlock(in_channels=64, out_channels=64)
-
-X = torch.randn((1, 64, 32, 32))
-X = block(X)
-
-print(X.shape)
